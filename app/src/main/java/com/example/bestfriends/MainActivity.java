fc5eccea07a5c3c1183cc070adda50d2bf3ce82e
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-14 16:23:29 -0600

    checks for becca mia

--- app/src/main/java/com/example/bestfriends/MainActivity.java
+++ app/src/main/java/com/example/bestfriends/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView isBestFriend;
+    private String nameEntered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +22,61 @@ public class MainActivity extends AppCompatActivity {
         Button enterButton = (Button)findViewById(R.id.enterButton);
 
         //name editText set up
-        EditText name = (EditText) findViewById(R.id.name);
+        final EditText name = (EditText) findViewById(R.id.name);
 
         //Dialog for entering name
         enterButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // name is entered
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                final View friendStatusView = getLayoutInflater().inflate(R.layout.is_best_friend, null);
 
-                //build and show popup
-                builder.setView(friendStatusView);
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                //check whos name it is (TODO: make into a database of friends!)
+                if(name.getText().toString().equals("becca")
+                        ||name.getText().toString().equals("Becca")
+                        ||name.getText().toString().equals("Rebecca")
+                        ||name.getText().toString().equals("rebecca")
+                        ||name.getText().toString().equals("Becca Fung")
+                        ||name.getText().toString().equals("Rebecca Fung")
+                        ||name.getText().toString().equals("becca fung")
+                        ||name.getText().toString().equals("rebecca fung"))  {
+
+                    // name is entered
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    final View friendStatusView = getLayoutInflater().inflate(R.layout.is_best_friend, null);
+
+                    //build and show popup
+                    builder.setView(friendStatusView);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } else if(name.getText().toString().equals("mia")
+                        ||name.getText().toString().equals("Mia")
+                        ||name.getText().toString().equals("maya")
+                        ||name.getText().toString().equals("Maya")
+                        ||name.getText().toString().equals("Mia Ramsbottom")
+                        ||name.getText().toString().equals("Maya Ramsbottom")
+                        ||name.getText().toString().equals("mia ramsbottom")
+                        ||name.getText().toString().equals("maya ramsbottom")){
+
+                    // name is entered
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    final View friendStatusView = getLayoutInflater().inflate(R.layout.is_girlfriend, null);
+
+                    //build and show popup
+                    builder.setView(friendStatusView);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } else {
+
+                    // name is entered
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    final View friendStatusView = getLayoutInflater().inflate(R.layout.is_not_best_friend, null);
+
+                    //build and show popup
+                    builder.setView(friendStatusView);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                }
             }
         });
 
