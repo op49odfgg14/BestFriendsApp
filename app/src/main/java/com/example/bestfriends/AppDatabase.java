6886b7bd8f0827163a64b61d6ba50928b0154dab
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-14 23:50:06 -0600

    AppDatabase implemented as well as database instantiated!

---
+++ app/src/main/java/com/example/bestfriends/AppDatabase.java
@@ -0,0 +1,17 @@
+package com.example.bestfriends;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+
+/**
+ * Created by op49o_000 on 2018-03-14.
+ */
+
+@Database(entities = {User.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract UserDao userDao();
+
+    AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+            AppDatabase.class, "database-name").build();
+}
