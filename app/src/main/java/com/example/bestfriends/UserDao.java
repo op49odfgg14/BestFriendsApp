70393ff6e1a1d4181c376bb86194681cfc4278a9
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-14 23:43:35 -0600

    implemented rough Data Access Objects interface

---
+++ app/src/main/java/com/example/bestfriends/UserDao.java
@@ -0,0 +1,31 @@
+package com.example.bestfriends;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by op49o_000 on 2018-03-14.
+ */
+
+@Dao
+public interface UserDao {
+    @Query("SELECT * FROM user")
+    List<User> getAll();
+
+    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM user WHERE first_name LIKE :first AND "
+            + "last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(List<User> users);
+
+    @Delete
+    void delete(User user);
+}
