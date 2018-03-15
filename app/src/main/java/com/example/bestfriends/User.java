c8620b4990b298ad382ee926130d7fbbd6d9eab6
Alden Emerson Ern Tan <aet@ualberta.ca>
2018-03-14 23:27:00 -0600

    user table made!

---
+++ app/src/main/java/com/example/bestfriends/User.java
@@ -0,0 +1,60 @@
+package com.example.bestfriends;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by op49o_000 on 2018-03-14.
+ */
+
+
+
+@Entity
+public class User {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    @ColumnInfo(name = "message")
+    private String message;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
+
